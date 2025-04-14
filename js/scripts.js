61e0dac7afa62cd5cd1778a1778c347a0d344974
Ifeanyi Eseni <barnabaseseni@gmail.com>
2025-04-14 07:03:25 +0100

    about and services page completed

--- js/scripts.js
+++ js/scripts.js
@@ -599,3 +599,5 @@
 		$(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({default_offset_pct: 0.7});
 		$(".twentytwenty-container[data-orientation='vertical']").twentytwenty({default_offset_pct: 0.3, orientation: 'vertical'});
 	}
+	
+	 document.getElementById('year').textContent = new Date().getFullYear()	
